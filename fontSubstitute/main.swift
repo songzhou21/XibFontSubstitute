9ed83b9b40148b6325ebf5a93fa710f56f83493b
Song Zhou <song.zhou@pdabc.com>
2020-03-17 17:40:44 +0800

    支持修改单个 xib

--- fontSubstitute/main.swift
+++ fontSubstitute/main.swift
@@ -8,5 +8,174 @@
 
 import Foundation
 
-print("Hello, World!")
+struct XibTag {
+    let elementName: String
+    let attributeDict: [String : String]
+    let selfClosingTag: Bool
+    let lineNumber: Int
+}
+
+class XibParser: NSObject, XMLParserDelegate {
+    init?(filePath: String, targetElement: String, completion: @escaping (_ parser: XibParser, _ tags: [XibTag]) -> ()) {
+        self.filePath = filePath
+        guard let parser = XMLParser(contentsOf: URL(fileURLWithPath: filePath)) else {
+            return nil
+        }
+
+        self.xmlParser = parser
+        self.targetElement = targetElement
+        self.completion = completion
+        tags = []
+        
+        super.init()
+        self.xmlParser.delegate = self
+    }
+    
+    let filePath: String
+    let xmlParser: XMLParser
+    let targetElement: String
+    var tags: [XibTag]
+    let completion: ((_ parser: XibParser, _ tags: [XibTag]) -> ())?
+    
+    func parserDidStartDocument(_ parser: XMLParser) {
+        print("start parse \(filePath)")
+    }
+    
+    func parser(_ parser: XMLParser, foundUnparsedEntityDeclarationWithName name: String, publicID: String?, systemID: String?, notationName: String?) {
+        print(name)
+    }
+
+    func parser(_ parser: XMLParser, foundElementDeclarationWithName elementName: String, model: String) {
+        print(elementName)
+    }
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        if elementName == self.targetElement {
+            let tag = XibTag(elementName: elementName, attributeDict: attributeDict, selfClosingTag: true, lineNumber: parser.lineNumber)
+            tags.append(tag)
+        }
+    }
+    
+    func parserDidEndDocument(_ parser: XMLParser) {
+        print("end parse \(filePath)")
+        self.completion?(self, self.tags)
+    }
+    
+    func parse() {
+        xmlParser.parse()
+    }
+}
+
+struct XibSerializer {
+    func makeTagString(elementName: String, attributeDict: [String : String], selfClosing: Bool) -> String {
+        guard selfClosing == true else {return ""}
+        
+        let attributes =
+        attributeDict.reduce("") { (result, e) -> String in
+            let (key, value) = e
+
+            return result + " \(key)=\"\(value)\""
+        }
+        
+        return "<\(elementName)\(attributes)/>"
+    }
+    
+    func output() -> String {
+        let contents = try! String(contentsOfFile: filePath)
+        let lines = contents.split(separator: "\n")
+
+        var dict = [Int: XibTag]()
+        tags.forEach { (tag) in
+            dict[tag.lineNumber] = tag
+        }
+
+        var newContens = ""
+        for (index, line) in lines.enumerated() {
+            let linNum = index+1
+
+            if let tag = dict[linNum] {
+                let newAttributes = self.attributesModifier(tag)
+                if newAttributes.1 == false {
+                    continue
+                }
+                
+                let newLine = makeTagString(elementName: tag.elementName, attributeDict: newAttributes.0, selfClosing: tag.selfClosingTag)
+                print("change line:\(tag.lineNumber) \(line) \n -> \(newLine)")
+                newContens.append(contentsOf: newLine + "\n")
+            } else {
+                newContens.append(contentsOf: line + "\n")
+            }
+        }
+        
+        return newContens
+    }
+    
+    let filePath: String
+    let tags: [XibTag]
+    let attributesModifier: (_ tag: XibTag) -> ([String: String], Bool)
+}
+
+let xibFile = CommandLine.arguments[1]
+
+let font = [
+    "regular": [
+        "name": "FZLANTY_JW--GB1-0",
+        "family": "FZLanTingYuanS-R-GB",
+    ],
+    "bold": [
+        "name": "FZLANTY_CUJW--GB1-0",
+        "family": "FZLanTingYuanS-B-GB",
+    ],
+]
+
+let pingfangRegular = "PingFangSC-Regular"
+let pingfangBold = "PingFangSC-Medium"
+
+let pingfangFonts = [
+    "PingFangSC-Ultralight",
+    "PingFangSC-Light",
+    "PingFangSC-Thin",
+    "PingFangSC-Regular",
+    "PingFangSC-Medium",
+    "PingFangSC-Semibold",
+]
+
+let pingfangRegularIdx = pingfangFonts.firstIndex(of: "PingFangSC-Regular")!
+
+let parser = XibParser(filePath: xibFile, targetElement: "fontDescription") { parser, tags in
+    let serializer = XibSerializer(filePath: parser.filePath, tags: tags) { tag in
+        var dict = tag.attributeDict
+
+        var needReplace = false
+        /// 支持替换的字体
+        if let name = dict["name"], let idx = pingfangFonts.firstIndex(of: name) {
+            needReplace = true
+            let targetFont: [String: String]
+            if idx <= pingfangRegularIdx { // 小于等于 regular 字重
+                targetFont = font["regular"]!
+            } else  { // 大于等于 bold 自重
+                targetFont = font["bold"]!
+            }
+
+            targetFont.forEach { key, value in
+                dict[key] = value
+            }
+        }
+
+        return (dict, needReplace)
+    }
+    
+    print("start Serialize \(xibFile)")
+    let output = serializer.output()
+
+    let url = URL(fileURLWithPath: xibFile)
+    do {
+        try output.write(to: url, atomically: false, encoding: .utf8)
+    } catch {
+        print(error)
+    }
+    print("end Serialize \(xibFile)")
+}
+
+parser?.parse()
 
