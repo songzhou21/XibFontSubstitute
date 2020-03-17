370405daf2357e9f888f8fb6958afca130d41d2d
Song Zhou <song.zhou@pdabc.com>
2020-03-17 20:06:39 +0800

    支持扫描目录

--- fontSubstitute/main.swift
+++ fontSubstitute/main.swift
@@ -95,16 +95,15 @@ struct XibSerializer {
 
             if let tag = dict[linNum] {
                 let newAttributes = self.attributesModifier(tag)
-                if newAttributes.1 == false {
+                if newAttributes.1 == true {
+                    let newLine = makeTagString(elementName: tag.elementName, attributeDict: newAttributes.0, selfClosing: tag.selfClosingTag)
+                    print("change line:\(tag.lineNumber) \(line) \n -> \(newLine)")
+                    newContens.append(contentsOf: newLine + "\n")
                     continue
                 }
-                
-                let newLine = makeTagString(elementName: tag.elementName, attributeDict: newAttributes.0, selfClosing: tag.selfClosingTag)
-                print("change line:\(tag.lineNumber) \(line) \n -> \(newLine)")
-                newContens.append(contentsOf: newLine + "\n")
-            } else {
-                newContens.append(contentsOf: line + "\n")
             }
+            
+            newContens.append(contentsOf: line + "\n")
         }
         
         return newContens
@@ -115,8 +114,7 @@ struct XibSerializer {
     let attributesModifier: (_ tag: XibTag) -> ([String: String], Bool)
 }
 
-let xibFile = CommandLine.arguments[1]
-
+/// 替换后的字体，目前只有两个字重，name，family 和 xib 中的 tag 中的属性是对应的
 let font = [
     "regular": [
         "name": "FZLANTY_JW--GB1-0",
@@ -142,40 +140,110 @@ let pingfangFonts = [
 
 let pingfangRegularIdx = pingfangFonts.firstIndex(of: "PingFangSC-Regular")!
 
-let parser = XibParser(filePath: xibFile, targetElement: "fontDescription") { parser, tags in
-    let serializer = XibSerializer(filePath: parser.filePath, tags: tags) { tag in
-        var dict = tag.attributeDict
-
-        var needReplace = false
-        /// 支持替换的字体
-        if let name = dict["name"], let idx = pingfangFonts.firstIndex(of: name) {
-            needReplace = true
-            let targetFont: [String: String]
-            if idx <= pingfangRegularIdx { // 小于等于 regular 字重
-                targetFont = font["regular"]!
-            } else  { // 大于等于 bold 自重
-                targetFont = font["bold"]!
+func parserWithPath(filePath: String) -> XibParser? {
+    let parser = XibParser(filePath: filePath, targetElement: "fontDescription") { parser, tags in
+        let serializer = XibSerializer(filePath: parser.filePath, tags: tags) { tag in
+            var dict = tag.attributeDict
+            
+            var needReplace = false
+            /// 支持替换的字体
+            if let name = dict["name"], let idx = pingfangFonts.firstIndex(of: name) {
+                needReplace = true
+                let targetFont: [String: String]
+                if idx <= pingfangRegularIdx { // 小于等于 regular 字重
+                    targetFont = font["regular"]!
+                } else  { // 大于等于 bold 自重
+                    targetFont = font["bold"]!
+                }
+                
+                targetFont.forEach { key, value in
+                    dict[key] = value
+                }
             }
-
-            targetFont.forEach { key, value in
-                dict[key] = value
-            }
+            
+            return (dict, needReplace)
         }
-
-        return (dict, needReplace)
+        
+        print("start Serialize \(filePath)")
+        let output = serializer.output()
+        
+        let url = URL(fileURLWithPath: filePath)
+        do {
+            try output.write(to: url, atomically: false, encoding: .utf8)
+        } catch {
+            print(error)
+        }
+        print("end Serialize \(filePath)")
     }
     
-    print("start Serialize \(xibFile)")
-    let output = serializer.output()
-
-    let url = URL(fileURLWithPath: xibFile)
-    do {
-        try output.write(to: url, atomically: false, encoding: .utf8)
-    } catch {
-        print(error)
-    }
-    print("end Serialize \(xibFile)")
+    parser?.parse()
+    
+    return parser
 }
 
-parser?.parse()
+func generateWithPath(path: String, recursive: Bool) {
+    let fileUrl = URL(fileURLWithPath: path)
+    var parsers = [XibParser?]()
+    if fileUrl.hasDirectoryPath {
+        
+        let closure: (String) -> () = { file in
+            let fullPath = fileUrl.appendingPathComponent(file)
+            
+            guard fullPath.hasDirectoryPath == false,
+                fullPath.pathExtension == "xib" else { return }
+            
+            parsers.append(parserWithPath(filePath: fullPath.path))
+        }
+        
+        let files: [String]
+        if recursive {
+            files = try! FileManager.default.subpathsOfDirectory(atPath: path)
+        } else {
+            files = try! FileManager.default.contentsOfDirectory(atPath: path)
+        }
+    
+        for file in files {
+            closure(file)
+        }
+    } else {
+        parsers.append(parserWithPath(filePath: fileUrl.path))
+    }
+}
 
+let help = """
+使用：fontSubstitute [-Options...] 文件/文件夹路径
+
+Options:
+-h      帮助信息
+
+-r      递归搜索
+"""
+
+func main() {
+    guard CommandLine.arguments.count >= 2 else {
+        print(help)
+        return
+    }
+    
+    let arg1 = CommandLine.arguments[1]
+    if arg1[arg1.startIndex] == "-" {
+        let option = arg1[arg1.index(after: arg1.startIndex)]
+
+        switch option {
+        case "r":
+            guard CommandLine.arguments.count == 3 else {
+                print(help)
+                return
+            }
+            
+            generateWithPath(path: CommandLine.arguments[2], recursive: true)
+        default:
+            print(help)
+            return
+        }
+    } else {
+         generateWithPath(path: arg1, recursive: false)
+    }
+}
+
+main()
